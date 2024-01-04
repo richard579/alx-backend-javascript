eb45d1d21cf5803cf5855f83095ed36b2b2e8368
richard579 <richardbeng358@outlook.com>
2024-01-04 13:42:25 +0000

    5. A Building

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -2,9 +2,9 @@ class Building {
   constructor(sqft) {
     if (this.constructor !== Building) {
       if (typeof this.evacuationWarningMessage !== 'function') {
-        throw new Error (
+        throw new Error(
           'Class extending Building must override evacuationWarningMessage',
-	);
+        );
       }
     }
     this.sqft = sqft;
