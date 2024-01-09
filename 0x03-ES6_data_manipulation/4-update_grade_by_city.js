cd1a3bffdffc18e7a0655d3e0dd1467424fd6383
richard579 <richardbeng358@outlook.com>
2024-01-09 10:01:01 +0000

    4. Combine

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -2,7 +2,7 @@
 function updateStudentGradeByCity(students, city, newGrades) {
     const studentsInCity = students.filter((student) => student.location === city);
     return studentsInCity.map((student) => {
-      const filteredGrades = newGrades.filter((grade) => grade.studentId === student.Id);
+      const filteredGrades = newGrades.filter((grade) => grade.studentId === student.id);
       if (filteredGrades.length > 0) {
         return {
           ...student,
