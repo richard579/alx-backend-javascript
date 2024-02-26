31683b426451ea1fa6e33a932495dc0f785a3dcf
richard579 <richardbeng358@outlook.com>
2024-02-26 12:50:14 +0000

    0. Executing basic javascript with Node JS

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,15 @@
+/**
+ * Logs a message to the console.
+ * @param {string} message - the message to log
+ * @returns {void}
+ */
+// Arrow function
+const displayMessage = (message) => console.log(message);
+
+/**
+ * Exports the displayMessage funtion
+ * @module displayMessage
+ * @function
+ */
+// Default module export
+module.exports = displayMessage;
