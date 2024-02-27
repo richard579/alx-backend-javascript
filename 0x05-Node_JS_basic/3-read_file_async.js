02e5eaa8c0373eccaa741e506b34a2bc8c4a681f
richard579 <richardbeng358@outlook.com>
2024-02-27 12:49:40 +0000

    3-read_file_async.js

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -16,23 +16,23 @@ function countStudents(fileName) {
             const field = lines[i].toString().split(',');
             if (Object.prototype.hasOwnProperty.call(students, field[3])) {
               students[field[3]].push(field[0]);
-	    } else {
+            } else {
               students[field[3]] = [field[0]];
-	    }
+            }
             if (Object.prototype.hasOwnProperty.call(fields, field[3])) {
               fields[field[3]] += 1;
-	    } else {
+            } else {
               fields[field[3]] = 1;
-	    }
-	  }
-	}
+            }
+          }
+        }
         const l = length - 1;
         console.log(`Number of students: ${l}`);
         for (const [key, value] of Object.entries(fields)) {
           if (key !== 'field') {
             console.log(`Number of students in ${key}: ${value}. List: ${students[key].join(', ')}`);
-	  }
-	}
+          }
+        }
         resolve(data);
       }
     });
